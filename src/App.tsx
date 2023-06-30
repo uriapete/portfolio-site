8b77d473e5f77a748350b290b36eb99e4442060e
uriapete <uriarteangelo@gmail.com>
2023-06-30 15:09:13 -0700

    add topbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
+import TopBar from './components/TopBar';
 
 function App() {
   return (
     <div className="App">
+      <TopBar />
       <Home />
     </div>
   );
