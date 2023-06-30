0d5a53634e61d001546e31f5410d1b5fb276cf2f
uriapete <uriarteangelo@gmail.com>
2023-06-30 15:48:44 -0700

    second div layer for top-bar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -4,9 +4,11 @@ import './styles/TopBar.scss'
 export default function TopBar():ReactElement{
     return(
         <div className="comp-top-bar">
-            <nav className="top-bar-nav">
-                <a href="/">Home</a>
-            </nav>
+            <div className="top-bar">
+                <nav className="top-bar-nav">
+                    <a href="/">Home</a>
+                </nav>
+            </div>
         </div>
     )
 }
