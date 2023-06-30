bcfa041d3c4800158f56e909ec90daacb3a29ab4
uriapete <uriarteangelo@gmail.com>
2023-06-30 15:18:04 -0700

    comment out unused vars, app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
 import TopBar from './components/TopBar';
